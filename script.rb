c9c86c701361d6f236baf825c4f75e15a86e354e
AndriusJur <58996191+AndriusJur@users.noreply.github.com>
2025-06-17 16:25:55 +0300

    Init commit

--- script.rb
+++
@@ -1,4 +0,0 @@
-puts "please enter your name"
-your_name = gets.chomp
-my_name = "just a String"
-puts("Hello, #{your_name}! I'm " + my_name)
---
+++ strings.rb
@@ -0,0 +1,10 @@
+my_name = "Kwyjibo"
+
+puts "please enter your name"
+your_name = gets.chomp.downcase.strip
+puts your_name.include?"x" #contains in Java
+puts your_name.length()
+puts your_name[0,3]
+puts your_name.index("i")
+puts your_name[0].upcase ()
+puts("Hello, #{your_name}! I'm "  + my_name ) #shovel operator
