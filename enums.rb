e38c776eb7b8477e8ce2687eb3dcb85503df59eb
AndriusJur <58996191+AndriusJur@users.noreply.github.com>
2025-06-24 13:08:32 +0300

    Practising enums.

--- enums.rb
+++ enums.rb
@@ -5,3 +5,13 @@ phone_codes.each { |key, value| puts "#{key} is #{value}" }
 phone_codes.keys.map { |key| key.downcase }
 
 phone_codes.each { |key, value| puts "#{key} is #{value}" }
+
+
+postal_codes = ["031021", "3102103210", "3103130", "321032", "321032"]
+postal_codes.each_with_index{|code, index| puts code if index.even? }
+
+postal_codes.map do |code|
+  code.to_i
+
+end
+
